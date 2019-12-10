df12ac03f4f6afaba13989df5735b7b7b01fe6f1
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-12-10 18:20:14 +0100

    feat(Box): make the box component skeleton

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,9 +1,22 @@
 import React, { memo } from 'react';
 import { string, node } from 'prop-types';
+import Text from '../Text';
+import './Box.css';
 
 const Box = props => {
+  const { title, extra, children } = props;
+
   return (
-    <div>I'm a box</div>
+    <div className="box">
+      <div className="box__info">
+        <Text tag="h4">{title}</Text>
+        <div>{extra}</div>
+      </div>
+      <div className="box__content">
+        {children}
+      </div>
+    </div>
+    
   )
 }
 
--- src/components/Box/Box.stories.js
+++ src/components/Box/Box.stories.js
@@ -4,3 +4,8 @@ import Box from './';
 export default { title: 'Box' };
 
 export const empty = () => <Box />;
+export const withTitle = () => (
+  <Box title="title" extra={<p>This is extra content</p>}>
+    <p>I'm Content</p>
+  </Box>
+);
