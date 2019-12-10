6f46c9069120603e8d05e40f3025ddd0c38e4dc2
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-12-11 00:55:05 +0100

    feat(Box): add line Chart Story

--- src/components/Box/Box.stories.js
+++ src/components/Box/Box.stories.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Box from "./";
 import Text from "../Text";
-import Pagination from '../Pagination';
+import Select from '../Select';
+import Pagination from "../Pagination";
 import ReactHighcharts from "react-highcharts";
 
 export default { title: "Box" };
@@ -53,6 +54,45 @@ const ballStyle = {
   fontSize: "1.5em"
 };
 
+const lineConfig = {
+  title: '',
+  xAxis: {
+    categories: ['8:00 AM', '9:00 AM', '10:00 AM','11:00 AM','12:00 PM','1:00 PM','2:00 PM','3:00 PM','4:00 PM','5:00 PM', '6:00 PM' ]
+  },
+  yAxis: {
+    title: {
+      text: ""
+    }
+  },
+  tooltip: {
+    enabled: false,
+  },
+  plotOptions: {
+    line: {
+      marker: {
+        enabled: false,
+      }
+    },
+    series: {
+      color: '#FF0000'
+    }
+  },
+  series: [
+    {
+      name: "Today",
+      data: [12, 30, 50, 122, 133, 167, 155, 143, 122, 98, 44]
+    }
+  ]
+};
+
+const extraContent = (
+  <div>
+    <Select values={['All', 1, 2, 3, 4]} label="Floor" />
+    <Select values={['All', 1, 2, 3, 4]} label="Entrance" />
+    <Select values={['Gender', 'Male', 'Female']} label="Split By" size="large" />
+  </div>
+)
+
 export const empty = () => <Box />;
 export const withTitle = () => (
   <Box title="title" extra={<p>This is extra content</p>}>
@@ -69,15 +109,34 @@ export const withPieChart = () => (
 export const withPagination = () => (
   <Box title="Age" extra={<Text color="gray">(This is dwell time)</Text>}>
     <div style={{ marginTop: 10 }}>
-      <div style={ballStyle}>14 min</div><br/>
+      <div style={ballStyle}>14 min</div>
+      <br />
       <div>
-        <Text color="gray" size="small">Floor</Text><br/>
-        <Pagination initialCheck={3} collection={[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}/>
+        <Text color="gray" size="small">
+          Floor
+        </Text>
+        <br />
+        <Pagination
+          initialCheck={3}
+          collection={[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        />
       </div>
       <div>
-        <Text color="gray" size="small">Zones</Text><br/>
-        <Pagination initialCheck={0} collection={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}/>
+        <Text color="gray" size="small">
+          Zones
+        </Text>
+        <br />
+        <Pagination
+          initialCheck={0}
+          collection={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+        />
       </div>
     </div>
+  </Box>
+);
+
+export const withLine = () => (
+  <Box title="Attendance" extra={extraContent}>
+    <ReactHighcharts config={lineConfig} />
   </Box>
 );
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,15 +1,20 @@
 import React, { memo } from 'react';
+import classNames from "classnames";
 import { array, string } from "prop-types";
 import Text from '../Text';
 import './Select.css';
 
 const Select = props => {
-  const { values, label } = props;
+  const { values, label, size } = props;
+
+  const classes = classNames('select-container__select', {
+    [`select-container__select--${size}`]: size
+  });
 
   return (
     <div className="select-container">
       <Text >{label}</Text>
-      <select className="select-container__select">
+      <select className={classes} >
       { values.map((item, index) => <option key={index} value={index}>{item}</option>) }
       </select>
     </div>
@@ -18,12 +23,13 @@ const Select = props => {
 
 Select.propTypes = {
   values: array,
+  size: string,
   label: string,
 };
 
 Select.defaultProps = {
   values: [],
+  size: 'medium',
 };
 
 export default memo(Select);
-
